58a3e6bd6234e4f00eae02b251f7ca0e6a8efbbf
Woong <parkpkww@gmail.com>
2025-01-07 17:57:19 +0900

    . router base name 설정

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import PlayPage from "./pages/PlayPage";
 
 function App() {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <>
                 <Header/>
                 <Routes>
