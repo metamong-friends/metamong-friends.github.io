8fcd01ca11de4e917fdd9747181bfb998352b067
Woong <parkpkww@gmail.com>
2025-01-07 17:44:43 +0900

    . PlayPage 구성  - WebGL 배포 페이지

--- src/pages/PlayPage.js
+++ src/pages/PlayPage.js
@@ -1,9 +1,18 @@
-const PlayPage = () => {
-    return (
-        <div className="HomePage-Body">
-            let's play
-        </div>
+import {Unity, useUnityContext} from "react-unity-webgl";
 
+const PlayPage = () => {
+
+    const {unityProvider} = useUnityContext({
+        loaderUrl: "build/Metamong.loader.js",
+        dataUrl: "build/Metamong.data",
+        frameworkUrl: "build/Metamong.framework.js",
+        codeUrl: "build/Metamong.wasm",
+    });
+
+    return (
+        <div className="PlayPage-Content">
+            <Unity unityProvider={unityProvider}/>
+        </div>
     );
 }
 
